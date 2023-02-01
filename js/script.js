004cbcbcfe6a89a51a189cc8904604f3f913ea55
faroukmanai <79013591+faroukmanai@users.noreply.github.com>
2023-01-31 21:19:23 -0500

    derniere version tp3

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/* Carousel faite avec swiperjs */
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
